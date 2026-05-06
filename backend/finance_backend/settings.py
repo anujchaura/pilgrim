e259dd3aa8572c4c18aebc337dbdd643e6580a95
unknown <shalinichaurasia67@gmail.com>
2026-05-06 18:20:20 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- backend/finance_backend/settings.py
+++ backend/finance_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h0i(!r!xlj1o6)rvoeiuc^$ds+tnl@#v@1&bg_n0rjc4emgh2h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
